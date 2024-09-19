e1f29abdacafd1b228565252ff27f39d17169191
onepunchkim <hyukjin4711@gmail.com>
2024-09-19 18:15:35 +0900

    string study

---
+++ src/lang/string/test/TestString7.java
@@ -0,0 +1,9 @@
+package lang.string.test;
+
+public class TestString7 {
+    public static void main(String[] args) {
+        String str = "      Hello Java  ";
+        String original = str.trim();
+        System.out.println("original = " + original);
+    }
+}
