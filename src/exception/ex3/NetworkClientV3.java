f16a66c91d1c75f9c0c5a18e3def1ffa44f0edd6
onepunchkim <hyukjin4711@gmail.com>
2024-10-05 16:47:34 +0900

    exception study

--- src/exception/ex3/NetworkClientV3.java
+++ src/exception/ex3/NetworkClientV3.java
@@ -1,28 +1,28 @@
 package exception.ex3;
 
-public class NetworkClientV4 {
+import exception.ex3.exception.ConnectExceptionV3;
+import exception.ex3.exception.SendExceptionV3;
+
+public class NetworkClientV3 {
     private final String address;
     public boolean connectError;
     public boolean sendError;
 
-    public NetworkClientV4(String address) {
+    public NetworkClientV3(String address) {
         this.address = address;
     }
 
-    public void connect() throws NetworkClientExceptionV3 {
+    public void connect() throws ConnectExceptionV3 {
         if(connectError) {
-            throw new NetworkClientExceptionV3("connectError", address + " 서버 연결 실패");
+            throw new ConnectExceptionV3(address, address + " 서버 연결 실패");
         }
         //연결 성공
         System.out.println(address + " 서버 연결 성공");
     }
 
-    public void send(String data) throws NetworkClientExceptionV3 {
+    public void send(String data) throws SendExceptionV3 {
         if(sendError) {
-            throw new NetworkClientExceptionV3("sendError", address + " 서버에 데이터 전송 실패: " + data);
-
-            //중간에 다른 예외가 발생했다고 가정
-            //throw new RuntimeException("ex");
+            throw new SendExceptionV3(data, address + " 서버에 데이터 전송 실패: " + data);
         }
         //전송 성공
         System.out.println(address + " 서버에 데이터 전송: " + data);
--- src/exception/ex3/exception/SendExceptionV3.java
+++ src/exception/ex3/exception/SendExceptionV3.java
@@ -1,9 +1,9 @@
-package exception.ex3;
+package exception.ex3.exception;
 
 public class SendExceptionV3 extends NetworkClientExceptionV3 {
     private final String sendData;
 
-    public SendExceptionV3(String message, String sendData) {
+    public SendExceptionV3(String sendData, String message) {
         super(message);
         this.sendData = sendData;
     }
